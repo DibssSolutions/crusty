69687d2c87a6ce8965e1bf4819fd9596f6842e1e
Valerii Derevshchykov <kaktuz34@gmail.com>
2019-09-20 18:04:21 +0300

    upd

--- src/js/components/_menu.js
+++ src/js/components/_menu.js
@@ -1,15 +1,21 @@
-import { OPEN, BODY } from '../constants';
+import { OPEN, BODY, OVERFLOW_HIDDEN, HTML } from '../constants';
 
 const header = $('.js-header');
 const btn = $('.js-btn-menu');
 
 btn.on('click', function() {
-  (!header.hasClass(OPEN)) 
-    ? header.addClass(OPEN)
-    : header.removeClass(OPEN);
+  if (!header.hasClass(OPEN)) {
+    HTML.addClass(OVERFLOW_HIDDEN);
+    header.addClass(OPEN);
+  }
+  else {
+  	HTML.removeClass(OVERFLOW_HIDDEN);
+  	header.removeClass(OPEN);
+  }
 });
 
 BODY.on('click', e => {
   if ($(e.target).closest('.js-btn-menu').length) return;
   header.removeClass(OPEN);
+  HTML.removeClass(OVERFLOW_HIDDEN);
 });
